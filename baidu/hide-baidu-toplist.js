51f1e71b07d700609c7744f710133e816cbcd4cd
哆啦钢镚儿 <doracoin@qq.com>
2017-12-28 11:46:52 +0800

    Update hide-baidu-toplist.js
    更改了名称；新增了一个网站；

--- baidu/hide-baidu-toplist.js
+++ baidu/hide-baidu-toplist.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         隐藏Baidu垃圾搜索热点
+// @name         清除页面不喜欢的内容（自用工具）
 // @namespace    http://doracoin.me/2017/09/09/hide-baidu-toplist/
 // @version      1.0.0
 // @description  破J8百度
@@ -7,14 +7,25 @@
 // @match        http*://www.baidu.com/s?wd=*
 // @match        http*://www.baidu.com/s?*wd=*
 // @match        http*://www.baidu.com/baidu?wd=*
+// @match        http*://jiecaobao.com/*
 // @grant        none
 // ==/UserScript==
 (function() {
     'use strict';
-    // Your code here...
+    // Baidu
     var ht=document.getElementsByClassName("cr-content ");
-    console.log(ht);
-    ht[ht.length-1].style.display="none";
-    console.log("已关闭百度的搜索热点，过滤垃圾信息，维护上网环境");
+    console.log('baidu-hotlist: ' + ht);
+    if (ht.length > 0) {
+        ht[ht.length-1].style.display="none";
+        console.log("已关闭百度的搜索热点，过滤垃圾信息，维护上网环境");
+    }
+
+    // jiecaobao.com
+    var unlikeImg=document.getElementsByClassName("alignnone");
+    console.log('unlikeImg: ' + unlikeImg);
+    if (unlikeImg.length > 0){
+        unlikeImg[unlikeImg.length-1].src="http://wx4.sinaimg.cn/mw690/7eb932c3ly1fmwb9f12zlj205k05k750.jpg";
+        console.log("已替换不好看的配图");
+    }
 }
 )();
