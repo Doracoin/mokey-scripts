9f6c07aeaa99c0985d88c96fcd31d9941f661ff3
Doracoin <i@doracoin.me>
2018-06-11 10:10:00 +0800

    Update hide-baidu-toplist.js

--- baidu/hide-baidu-toplist.js
+++ baidu/hide-baidu-toplist.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Doracoin的自用工具
 // @namespace    https://greasyfork.org/scripts/36797-doracoin%E7%9A%84%E8%87%AA%E7%94%A8%E5%B7%A5%E5%85%B7
-// @version      1.4.5
+// @version      1.4.6
 // @description  清除某些页面自己不喜欢的内容，或更改某些网站的样式
 // @author       Doracoin
 // @match        http*://www.baidu.com/s?wd=*
@@ -114,6 +114,14 @@
         var csdn_edu = document.getElementsByClassName("edu-promotion");
         if (csdn_edu != null && csdn_edu.length > 0) {
             csdn_edu[0].style.display="none";
+        }
+	var loginDiv = document.getElementsByClassName("pulllog-box");
+        if (loginDiv != null && loginDiv.length > 0) {
+            loginDiv[0].style.display="none";
+        }
+        var btnReadMore = document.getElementById("btn-readmore");
+        if (btnReadMore !=null){
+            btnReadMore.click();
         }
     }
 })();
